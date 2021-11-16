3825791391648e07e06cc663a08c84fe9c2e5fa6
phamilton23 <60441850+phamilton23@users.noreply.github.com>
2021-11-16 16:21:00 +0000

    Delete Pilot Study Cut-Down directory

--- Pilot Study Cut-Down/scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 14 16:31:13 2020
-
-@author: WOODCOH
-"""
-
-
--- Pilot Study Cut-Down/scripts/normal_run.py
+++
@@ -1,7 +0,0 @@
-from pywr.core import Model
-
-def run(json_path, csv_path):
-    model = Model.load(json_path, solver='glpk-edge')
-    model.run()
-    df = model.to_dataframe()
-    df.to_csv(csv_path)
